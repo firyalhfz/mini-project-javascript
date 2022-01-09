5a41d031202aa8d3f132bf68c1005adb68905ad2
firyalhuwaida <iyaalfiryal@gmail.com>
2022-01-09 14:56:30 +0700

    dad jokes

---
+++ dad-jokes/script.js
@@ -0,0 +1,17 @@
+const jokee = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+async function generateJoke(){
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json()
+    jokee.innerHTML = data.joke
+}
