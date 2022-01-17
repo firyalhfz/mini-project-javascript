7489488917e16108f70ab39da22253f809af2f68
firyalhuwaida <iyaalfiryal@gmail.com>
2022-01-18 00:59:23 +0700

    progress steps

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -1,48 +1,49 @@
-const progress = document.getElementById("progress");
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
-const circles = document.querySelectorAll(".circle");
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
 
-let currentActive = 1;
+let currentActive = 1
 
-next.addEventListener("click", () => {
+next.addEventListener('click', () => {
     currentActive++
 
-    if(currentActive > circles.length){
+    if(currentActive > circles.length) {
         currentActive = circles.length
     }
 
     update()
 })
 
-prev.addEventListener('click', ()=>{
+prev.addEventListener('click', () => {
     currentActive--
 
-    if(currentActive < 1){
+    if(currentActive < 1) {
         currentActive = 1
     }
 
     update()
 })
 
-function update(){
+function update() {
     circles.forEach((circle, idx) => {
-        if(idx < currentActive){
+        if(idx < currentActive) {
             circle.classList.add('active')
-        }else{
+        } else {
             circle.classList.remove('active')
         }
     })
 
-    const actives = document.querySelectorAll('active')
-    progress.style.width = (actives.length - 1) / (circles.length - 1)
+    const actives = document.querySelectorAll('.active')
 
-    if(currentActive == 1){
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+    if(currentActive === 1) {
         prev.disabled = true
-    }else if(currentActive === circles.length){
+    } else if(currentActive === circles.length) {
         next.disabled = true
-    }else {
+    } else {
         prev.disabled = false
-        next.disabled = false;
-    } 
+        next.disabled = false
+    }
 }
