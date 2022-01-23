e0ed1e1b69787c2b3f7ea6441bc9ede9ab65e5dd
firyalhuwaida <iyaalfiryal@gmail.com>
2022-01-23 13:24:49 +0700

    add: script rorating nav

--- rotating-nav/script.js
+++ rotating-nav/script.js
@@ -2,6 +2,8 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
+// when click open, the container will show navigation
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
+// when click close, the container will close the nav
 close.addEventListener('click', () => container.classList.remove('show-nav'))
