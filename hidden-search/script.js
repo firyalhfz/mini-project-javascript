bcb55aee9e191fda03e1d6741bdf00c7516ef58b
firyalhuwaida <iyaalfiryal@gmail.com>
2022-01-23 13:53:44 +0700

    add: script and style hidden search

--- hidden-search/script.js
+++ hidden-search/script.js
@@ -2,6 +2,7 @@ const search = document.querySelector('.search')
 const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
 
+// when we click the button, so the search will active (doing the style depends on css)
 btn.addEventListener('click', () => {
     search.classList.toggle('active')
     input.focus()
