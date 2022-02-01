832c79ec5158e560ce29f36dd0efc4677c9c1af9
firyalhuwaida <iyaalfiryal@gmail.com>
2022-02-01 19:15:41 +0700

    add: script and style form wave

--- form-wave/script.js
+++ form-wave/script.js
@@ -2,7 +2,10 @@ const labels = document.querySelectorAll('.form-control label')
 
 labels.forEach(label => {
     label.innerHTML = label.innerText
+    // split make the word divided, e, m, a, i, l
     .split('')
+    // this make the label text become wave
     .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+    // join, nyatuin array
     .join('')
 })
