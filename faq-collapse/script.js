6a3292aee0d49c8072cf67e30da458c5c949fa67
firyalhuwaida <iyaalfiryal@gmail.com>
2022-02-02 16:34:06 +0700

    add: script faq-collapse

--- faq-collapse/script.js
+++ faq-collapse/script.js
@@ -1,7 +1,14 @@
-const toggles = document.querySelectorAll('.faq-toggle')
+/*
+1. Bring in toggle buttons (querySelectorAll)
+2. Loop Trough nodelist (forEach)
+3. Add click event (addEventListener)
+4. Toggle the active class on the parent node (.parentNode & classList.toggle)
+*/
 
-toggles.forEach(toggle => {
-    toggle.addEventListener('click', ()=> {
-        toggle.parentNode.classList.toggle('active')
-    })
-})
+const toggles = document.querySelectorAll(".faq-toggle");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
+  });
+});
