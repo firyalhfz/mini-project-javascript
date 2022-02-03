3d9e507961ade12ed7131e5445f01299a2827980
firyalhuwaida <iyaalfiryal@gmail.com>
2022-02-03 16:38:19 +0700

    add: script animated navigation

--- animated-navigation/script.js
+++ animated-navigation/script.js
@@ -1,4 +1,5 @@
 const toggle = document.getElementById('toggle')
 const nav = document.getElementById('nav')
 
+// when click the icon it will make the toggle add class active
 toggle.addEventListener('click', () => nav.classList.toggle('active'))
