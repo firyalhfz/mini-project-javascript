169488222cdb92b2ee982b39684dd65e31048f31
firyalhuwaida <iyaalfiryal@gmail.com>
2022-02-03 17:04:44 +0700

    add: script increment counter

--- increment-counter/script.js
+++ increment-counter/script.js
@@ -4,13 +4,17 @@ counters.forEach(counter => {
     counter.innerText = '0'
 
     const updateCounter = () => {
+        // getAtribute from data-target
         const target = +counter.getAttribute('data-target')
         const c = +counter.innerText
 
-        const increment = target / 200
+        // this for the speed of change number
+        const increment = target / 20
 
         if(c < target) {
+            // this make the number add 
             counter.innerText = `${Math.ceil(c + increment)}`;
+            // this make updateCounter slowly
             setTimeout(updateCounter, 1)
         }else{
             counter.innerText = target
