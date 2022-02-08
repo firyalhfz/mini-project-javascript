35422cf4b5e4d72fd6884e284b112e0556af4fed
firyalhuwaida <iyaalfiryal@gmail.com>
2022-02-08 16:21:51 +0700

    add: style and script content place holder

--- content-place-holder/script.js
+++ content-place-holder/script.js
@@ -8,8 +8,10 @@ const date = document.getElementById("date");
 const animated_bgs = document.querySelectorAll(".animated-bg");
 const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
 
+// set time 2 seconds before get the data
 setTimeout(getData, 2500);
 
+// get the data
 function getData() {
   header.innerHTML =
     '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />';
@@ -21,6 +23,7 @@ function getData() {
   name.innerHTML = "John Doe";
   date.innerHTML = "Oct 08, 2020";
 
+  // when we already get the data, remove the class of animated bg and text
   animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
   animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
 }
