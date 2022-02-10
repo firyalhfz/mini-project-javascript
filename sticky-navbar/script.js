47f8c4bd73536f414dbf9027134c3b1440cc64f9
firyalhuwaida <iyaalfiryal@gmail.com>
2022-02-10 22:04:38 +0700

    sticky nvbar

--- sticky-navbar/script.js
+++ sticky-navbar/script.js
@@ -3,7 +3,6 @@ window.addEventListener("scroll", fixNav);
 
 function fixNav() {
   // if the high of scroll is more than 150 then make the nav active if not remove
-
   if (window.scrollY > nav.offsetHeight + 150) {
     nav.classList.add("active");
   } else {
