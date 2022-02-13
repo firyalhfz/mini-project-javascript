989c3f3e54cc9301e2c82e37387453a6532efe9f
firyalhuwaida <iyaalfiryal@gmail.com>
2022-02-13 16:28:47 +0700

    add: script mobile nav

--- mobile-tab-nav/script.js
+++ mobile-tab-nav/script.js
@@ -1,6 +1,7 @@
 const contents = document.querySelectorAll('.content')
 const listItems = document.querySelectorAll('nav ul li')
 
+// when one of the item clicked, hide all content and item, after that, the item that clicked and the index that clicked add the class of active and show
 listItems.forEach((item, idx) => {
     item.addEventListener('click', () => {
         hideAllContents()
@@ -11,11 +12,12 @@ listItems.forEach((item, idx) => {
     })
 })
 
+// hide all content with remove the class show
 function hideAllContents() {
     contents.forEach(content => content.classList.remove('show'))
 }
 
-
+// hide all list item with remove the class active
 function hideAllItems() {
     listItems.forEach(item => item.classList.remove('active'))
 }
