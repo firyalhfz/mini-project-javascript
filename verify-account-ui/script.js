dd50553e387c82443590139063c4a9171af450f2
firyalhuwaida <iyaalfiryal@gmail.com>
2022-02-13 17:53:46 +0700

    add: scriot verify account ui

--- verify-account-ui/script.js
+++ verify-account-ui/script.js
@@ -4,10 +4,12 @@ codes[0].focus()
 
 codes.forEach((code, idx) => {
     code.addEventListener('keydown', (e) => {
+        // for move after type in first box, and move and move, add the index and save
         if(e.key >= 0 && e.key <=9) {
             codes[idx].value = ''
             setTimeout(() => codes[idx + 1].focus(), 10)
         } else if(e.key === 'Backspace') {
+            // if delete than the index erase 1
             setTimeout(() => codes[idx - 1].focus(), 10)
         }
     })
