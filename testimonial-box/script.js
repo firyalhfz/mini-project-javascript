72d37064e05159ccdacb7520ebd39ca718997a54
Firyal <97155805+firyalhuwaida@users.noreply.github.com>
2022-02-14 08:53:07 +0700

    update testimonial box switcher

--- testimonial-box/script.js
+++ testimonial-box/script.js
@@ -6,7 +6,7 @@ const role = document.querySelector('.role')
 
 const testimonials = [
   {
-    name: 'Miyah Myles',
+    name: 'Dean Umainah',
     position: 'Marketing',
     photo:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6',
@@ -14,35 +14,35 @@ const testimonials = [
       "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
   },
   {
-    name: 'June Cha',
+    name: 'Haura Salsabila',
     position: 'Software Engineer',
     photo: 'https://randomuser.me/api/portraits/women/44.jpg',
     text:
       'This guy is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!',
   },
   {
-    name: 'Iida Niskanen',
+    name: 'Nisfa',
     position: 'Data Entry',
     photo: 'https://randomuser.me/api/portraits/women/68.jpg',
     text:
       "This guy is a hard worker. Communication was also very good with him and he was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
   },
   {
-    name: 'Renee Sims',
+    name: 'Salma Nuha',
     position: 'Receptionist',
     photo: 'https://randomuser.me/api/portraits/women/65.jpg',
     text:
       "This guy does everything he can to get the job done and done right. This is the second time I've hired him, and I'll hire him again in the future.",
   },
   {
-    name: 'Jonathan Nunfiez',
+    name: 'Zaid Ilham',
     position: 'Graphic Designer',
     photo: 'https://randomuser.me/api/portraits/men/43.jpg',
     text:
       "I had my concerns that due to a tight deadline this project can't be done. But this guy proved me wrong not only he delivered an outstanding work but he managed to deliver 1 day prior to the deadline. And when I asked for some revisions he made them in MINUTES. I'm looking forward to work with him again and I totally recommend him. Thanks again!",
   },
   {
-    name: 'Sasha Ho',
+    name: 'Eren Yeager',
     position: 'Accountant',
     photo:
       'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?h=350&auto=compress&cs=tinysrgb',
@@ -50,7 +50,7 @@ const testimonials = [
       'This guy is a top notch designer and front end developer. He communicates well, works fast and produces quality work. We have been lucky to work with him!',
   },
   {
-    name: 'Veeti Seppanen',
+    name: 'Mikasa Ackerman',
     position: 'Director',
     photo: 'https://randomuser.me/api/portraits/men/97.jpg',
     text:
@@ -60,6 +60,7 @@ const testimonials = [
 
 let idx = 1
 
+// this function for updating the data testi
 function updateTestimonial() {
   const { name, position, photo, text } = testimonials[idx]
 
@@ -75,4 +76,5 @@ function updateTestimonial() {
   }
 }
 
+// every 10 sec update
 setInterval(updateTestimonial, 10000)
